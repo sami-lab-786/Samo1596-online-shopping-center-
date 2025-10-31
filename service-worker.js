a2af427a7c589059520689fa014fc129c3abbb35
sami-lab-786 <sami132667@gmail.com>
2025-10-31 12:54:32 +0500

    added service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,12 @@
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('samo-store').then(cache => {
-      return cache.addAll(['/', '/index.html']);
+      return cache.addAll(['./',
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json',
+        './shopping-icon.webp']);
     })
   );
 });
@@ -11,3 +16,4 @@ self.addEventListener('fetch', e => {
     caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
+
